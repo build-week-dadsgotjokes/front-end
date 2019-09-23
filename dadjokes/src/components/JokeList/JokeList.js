16818f64588fce7e04c25228895b72d0eeed3bfe
Anthony <anthony.kizer88@gmail.com>
2019-09-23 15:37:58 -0700

    test

--- dadjokes/src/components/JokeList/JokeList.js
+++ dadjokes/src/components/JokeList/JokeList.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Joke from "./Joke/Joke";
+import axios from "axios";
 
 const JokeList = () => {
+  const [joke, setJoke] = useState([]);
+
+  axios
+    .get("https://official-joke-api.appspot.com/random_ten")
+    .then(res => console.log(res.data));
   return <div>JokeList</div>;
 };
 
