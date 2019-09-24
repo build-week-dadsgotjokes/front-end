57d738b384c7d1abaa41a1014e5239ce837afb79
Justin Brandley <brandleyj@gmail.com>
2019-09-24 16:18:01 -0500

    started profile.js

--- dadjokes/src/components/JokeList/JokeList.js
+++ dadjokes/src/components/JokeList/JokeList.js
@@ -5,50 +5,50 @@ import AddJoke from "../AddJoke/AddJoke";
 import { Link } from "react-router-dom";
 
 function JokeList() {
-  const [jokes, setJokes] = useState([]);
+	const [jokes, setJokes] = useState([]);
 
-  useEffect(() => {
-    axios
-      .get(`https://api-dadjokes.herokuapp.com/jokes/public`)
-      .then(response => {
-        console.log("Jokes", response);
-        setJokes(response.data);
-      })
-      .catch(error => {
-        console.log("The data was not returned", error);
-      });
-  }, []);
+	useEffect(() => {
+		axios
+			.get(`https://api-dadjokes.herokuapp.com/jokes/public`)
+			.then(response => {
+				console.log("Jokes", response);
+				setJokes(response.data);
+			})
+			.catch(error => {
+				console.log("The data was not returned", error);
+			});
+	}, []);
 
-  ///renders AddJoke and profile link if user is logged in (profile link will be moved to navbar once ayomide pushes his branch.  Waiting for his push to avoid merge conflicts)
-  const userLoggedIn = () => {
-    return (
-      <div>
-        <Link to="/profile">Profile</Link>
-        <AddJoke />
-      </div>
-    );
-  };
+	///renders AddJoke and profile link if user is logged in (profile link will be moved to navbar once ayomide pushes his branch.  Waiting for his push to avoid merge conflicts)
+	const userLoggedIn = () => {
+		return (
+			<div>
+				<Link to="/profile">Profile</Link>
+				<AddJoke />
+			</div>
+		);
+	};
 
-  return (
-    <div>
-      {localStorage.getItem("token") ? (
-        userLoggedIn()
-      ) : (
-        <h2>Hi Hungry, I'm Dad</h2>
-      )}
+	return (
+		<div>
+			{localStorage.getItem("token") ? (
+				userLoggedIn()
+			) : (
+				<h2>Hi Hungry, I'm Dad</h2>
+			)}
 
-      {jokes.map(joke => {
-        return (
-          <Joke
-            id={joke.id}
-            key={joke.id}
-            setup={joke.setup}
-            punchline={joke.punchline}
-          />
-        );
-      })}
-    </div>
-  );
+			{jokes.map(joke => {
+				return (
+					<Joke
+						id={joke.id}
+						key={joke.id}
+						setup={joke.setup}
+						punchline={joke.punchline}
+					/>
+				);
+			})}
+		</div>
+	);
 }
 
 export default JokeList;
--- dadjokes/src/components/Profile/Profile.js
+++ dadjokes/src/components/Profile/Profile.js
@@ -1,19 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import JokeList from "../JokeList/JokeList";
 
 const Profile = props => {
-  console.log(props);
-  const logout = e => {
-    e.preventDefault();
-    localStorage.clear();
-    window.location.href = "/";
-  };
-  return (
-    <div>
-      hello user!<button onClick={logout}>Logout</button>
-      <Link to="/jokes">Joke List</Link>
-    </div>
-  );
+	console.log(props);
+	const logout = e => {
+		e.preventDefault();
+		localStorage.clear();
+		window.location.href = "/";
+	};
+	return (
+		<div>
+			hello user!<button onClick={logout}>Logout</button>
+			<Link to="/jokes">Joke List</Link>
+			<JokeList />
+		</div>
+	);
 };
 
 export default Profile;
